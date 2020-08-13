1173d4d7eae5aca62c9039297d2c653a5e1eedeb
thelittlecitizen13 <amitassa9@gmail.com>
2020-08-13 09:54:56 +0300

    PriorityQueue added

---
+++ PriorityQueue/PriorityQueue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PriorityQueue.Abstract;
+
+namespace PriorityQueue
+{
+    class PriorityQueue<T> : IPriorityQueue<T>
+    {
+        public int Count => throw new NotImplementedException();
+
+        public T Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Enqueue(uint priority, T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PriorityQueue/Program.cs
+++ PriorityQueue/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using PriorityQueue;
+using PriorityQueue.Abstract;
 
 namespace PriorityQueue
 {
